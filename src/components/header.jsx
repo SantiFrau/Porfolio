b316ea43f3fbf3ef2b5a7dc11c6aa50371536d7d
Santi <santiagofrau3@gmail.com>
2024-03-05 13:19:04 -0300

    cambio-perfil

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ export default function Header(){
         <header className="w-full h-full gap-3 flex flex-row flex-col items-center justify-center fondo-cont-100 rounded-lg">
             
             <div className="md:p-0 p-5">
-             <img className="md:w-32 w-24 rounded-full" src="public/media/perfil.webp" alt="" />
+             <img style={{aspectRatio:1/1}} className="md:w-32 w-24 object-cover rounded-full" src="public/media/perfil.webp" alt="" />
              </div>
              <div className="flex gap-12 md:gap-5">
                <div className="relative bottom-7 hover:bg-zinc-600 p-1 rounded-full">
